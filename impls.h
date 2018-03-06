8165d4d631e8470daa44445cbb0a1926e50c4ead
Derek Gaston <derek.gaston@inl.gov>
2018-03-06 16:14:04 -0700

    Move implementations to their own file

---
+++ impls.h
@@ -0,0 +1,21 @@
+#ifndef IMPLS_H
+#define IMPLS_H
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <chrono>
+
+#include "vectorclass.h"
+#include "vectormath_exp.h"
+#include "fmath.h"
+
+#define Real double
+#define VecSize 4
+#define NumValues 16
+
+void vectorizedExp(std::vector<Real> & vec, std::vector<Real> & out_vec);
+void normalExp(std::vector<Real> & vec, std::vector<Real> & out_vec);
+void fmathExp(std::vector<Real> & vec, std::vector<Real> & out_vec);
+
+#endif
